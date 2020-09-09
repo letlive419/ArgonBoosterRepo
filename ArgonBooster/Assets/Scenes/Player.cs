d33f585030f51650544a1c13b51fe20bfc8a2dc4
Elvis <letlive419@gmail.com>
2020-09-09 16:08:28 -0400

    added controls

---
+++ ArgonBooster/Assets/Scenes/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("meters per second")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("meters")] [SerializeField] float xMeters = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffSet = horizontalThrow * xSpeed * Time.deltaTime;
+        float rawX = transform.localPosition.x + xOffSet;
+        float newX = Mathf.Clamp(rawX, -xMeters, xMeters);
+        print(horizontalThrow);
+
+        transform.localPosition = new Vector3(newX, transform.localPosition.y, transform.localPosition.z);
+    }
+}
